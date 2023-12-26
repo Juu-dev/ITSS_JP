11d2bd069d00b5510622f94e378cfd8b6ab525ec
Juu-dev <thong.pm772002@gmail.com>
2023-12-27 00:23:47 +0700

    Feature: Change name app

--- src/app/components/Brand.jsx
+++ src/app/components/Brand.jsx
@@ -1,19 +1,19 @@
-import { Box, styled } from '@mui/material';
-import { MatxLogo } from 'app/components';
-import useSettings from 'app/hooks/useSettings';
-import { Span } from './Typography';
+import { Box, styled } from "@mui/material";
+import { MatxLogo } from "app/components";
+import useSettings from "app/hooks/useSettings";
+import { Span } from "./Typography";
 
 const BrandRoot = styled(Box)(() => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-  padding: '20px 18px 20px 29px',
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+  padding: "20px 18px 20px 29px",
 }));
 
 const StyledSpan = styled(Span)(({ mode }) => ({
   fontSize: 18,
-  marginLeft: '.5rem',
-  display: mode === 'compact' ? 'none' : 'block',
+  marginLeft: ".5rem",
+  display: mode === "compact" ? "none" : "block",
 }));
 
 const Brand = ({ children }) => {
@@ -26,11 +26,14 @@ const Brand = ({ children }) => {
       <Box display="flex" alignItems="center">
         <MatxLogo />
         <StyledSpan mode={mode} className="sidenavHoverShow">
-          AM(WIP)
+          ITSS - HUST
         </StyledSpan>
       </Box>
 
-      <Box className="sidenavHoverShow" sx={{ display: mode === 'compact' ? 'none' : 'block' }}>
+      <Box
+        className="sidenavHoverShow"
+        sx={{ display: mode === "compact" ? "none" : "block" }}
+      >
         {children || null}
       </Box>
     </BrandRoot>
